54099e6862718283687a23f79e45e1c1e23414de
Dominik Rimpf <dev@d-rimpf.de>
2019-09-29 04:11:32 +0200

    [RELEASE] Prepare for 0.1 pypi release

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name='vmail-manager',
     version='0.1',
-    pymodules=find_packages(),
-    include_package_data=True,
+    author='Dominik Rimpf',
+    author_email='dev@d-rimpf.de',
+    description='Handy cli interface to manage an vmail-sql-db.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/domrim/vmail-manager/',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     install_requires=[
         'click',
         'sqlalchemy',
